607121bd61d34a498f9ee65373001af64f4c95df
gustavo <gfran.93@gmail.com>
2025-02-12 15:41:53 -0300

    Improve tests

--- app/src/test/java/com/gfc/pokedex/domain/repository/PokemonRepositoryTest.kt
+++ app/src/test/java/com/gfc/pokedex/domain/repository/PokemonRepositoryTest.kt
@@ -1,51 +1,96 @@
 package com.gfc.pokedex.domain.repository
 
-import com.gfc.pokedex.data.remote.model.PokemonListItem
-import com.gfc.pokedex.data.remote.model.PokemonListResponse
+import androidx.work.WorkManager
+import com.gfc.pokedex.data.local.entities.PokemonEntity
+import com.gfc.pokedex.data.local.entities.PokemonWithDetails
+import com.gfc.pokedex.data.local.entities.toPokemon
+import com.gfc.pokedex.data.local.service.PokemonDao
 import com.gfc.pokedex.data.remote.service.PokeApiService
-import com.gfc.pokedex.domain.model.Pokemon
-import io.mockk.coEvery
-import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.junit4.MockKRule
 import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
-import org.junit.Assert.assertEquals
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 class PokemonRepositoryTest {
 
-    private val pokeApiService = mockk<PokeApiService>()
-    private val repository = PokemonRepository(pokeApiService)
+    @get:Rule
+    val mockkRule = MockKRule(this)
 
-    @Test
-    fun `empty list test`() = runTest {
-        coEvery {
-            pokeApiService.getPokemonList(any())
-        } returns PokemonListResponse(results = listOf())
+    private val pokeApiService: PokeApiService = mockk()
+    private val pokemonDao: PokemonDao = mockk()
+    private val workManager: WorkManager = mockk()
 
+    private val filesDirPath = "/path/to/files"
+    private val pokemonEntities = listOf(
+        PokemonEntity(
+            id = 1,
+            name = "Bulbasaur",
+            weight = 69,
+            height = 7,
+            baseExperience = 64,
+            specie = "Seed"
+        ),
+        PokemonEntity(
+            id = 2,
+            name = "Ivysaur",
+            weight = 130,
+            height = 10,
+            baseExperience = 142,
+            specie = "Seed"
+        ),
+        PokemonEntity(
+            id = 3,
+            name = "Venusaur",
+            weight = 1000,
+            height = 20,
+            baseExperience = 263,
+            specie = "Seed"
+        )
+    )
 
-        val response = repository.getPokemonList()
+    private lateinit var pokemonRepository: PokemonRepository
 
-        assertEquals(listOf<Pokemon>(), response)
-        coVerify(exactly = 1) { pokeApiService.getPokemonList(any()) }
+    @Before
+    fun setUp() {
+        pokemonRepository = PokemonRepository(pokeApiService, pokemonDao, workManager, filesDirPath)
     }
 
     @Test
-    fun `getPokemonList should return a list of Pokemon`() = runTest {
-        val pokemonResponse = PokemonListResponse(
-            results = listOf(
-                PokemonListItem(name = "bulbasaur", url = "https://pokeapi.co/api/v2/pokemon/1/"),
-                PokemonListItem(name = "ivysaur", url = "https://pokeapi.co/api/v2/pokemon/2/")
-            )
+    fun `getAllPokemon should return Flow of Pokemon from DAO`() = runTest {
+        val expectedPokemon = pokemonEntities.map { it.toPokemon() }
+
+        every { pokemonDao.getAllPokemon() } returns flowOf(pokemonEntities)
+
+        val result = pokemonRepository.getAllPokemon().first()
+
+        Assert.assertEquals(expectedPokemon, result)
+        verify { pokemonDao.getAllPokemon() }
+    }
+
+    @Test
+    fun `getPokemonById should return Flow of Pokemon`() = runTest {
+        val pokemonId = 1
+        val pokemonWithDetails = PokemonWithDetails(
+            pokemon = pokemonEntities.first(),
+            types = emptyList(),
+            abilities = emptyList(),
+        )
+        val expectedPokemon = pokemonWithDetails.toPokemon().copy(
+            imageFileName = "/path/to/files/pokemon_1.png"
         )
 
-        coEvery { pokeApiService.getPokemonList(any()) } returns pokemonResponse
+        every { pokemonDao.getPokemonWithDetails(pokemonId) } returns flowOf(pokemonWithDetails)
 
-        val result = repository.getPokemonList()
+        val result = pokemonRepository.getPokemonById(pokemonId).first()
 
-        // Assert
-        assertEquals(2, result.size)
-        assertEquals("Bulbasaur", result[0].name)
-        assertEquals("Ivysaur", result[1].name)
-        coVerify(exactly = 1) { pokeApiService.getPokemonList(any()) }
+        Assert.assertEquals(expectedPokemon, result)
+        verify { pokemonDao.getPokemonWithDetails(pokemonId) }
     }
 }
